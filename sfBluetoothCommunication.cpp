b55f66f316f66da7901c215eb91ad91c1ea87029
maitz285 <jannik.metzner@yahoo.de>
2015-07-25 09:10:28 +0000

    Adding Files

---
+++ sfBluetoothCommunication.cpp
@@ -0,0 +1,19 @@
+/*
+ * untitled.cxx
+ * 
+ * Copyright 2015  SynchroFlight
+ * 
+ * Author: Jannik Metzner <jametzne@htwg-konstanz.de>
+ * 
+ * 25/07/2015
+ */
+
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
